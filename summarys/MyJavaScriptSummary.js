5b1abe3fe75bedbe53ab35a6ef639208f98817cd
Ahmad Ismail <xahmad895@gmail.com>
2024-08-24 20:37:01 +0300

    i watched the first 10 videos in the java script and this is the first day

---
+++ summarys/MyJavaScriptSummary.js
@@ -0,0 +1,37 @@
+/*
+? how to Output something in js: there are a number of methods but the main is four 
+// 1) window.alert("anyMessage"); -> this method is not used any more it stops the flow of code.
+// 2) console.log("anyMessage");  -> this method will output on the console log screen.
+// 3) document.write("anyMessage"); 
+// -> document is the class of the html file it will write on the html file.
+// 4) document.writeln("anyMessage");
+# ---------------------------------------------------------------------------------------------
+? the Console have many functions i will write a number of these functions:
+// 1) console.clear();
+// 2) console.count("AnyThing");
+// 3) console.countReset("AnyThing");
+// 4) console.warn("AnyThing");
+// 5) console.error("AnyThing");
+// 6) console.info("AnyThing");
+// 7) console.log("AnyThing");
+// 8) console.table(["h" , "hi" , "hi"]);
+! console.error("%c %s Any Text %f %i " , "color:red;" , "use" , 3.9 , 0);
+// (%d or %i) -> Outputs an integer.
+// (%s) -> Outputs a string.
+// (%f) -> Outputs a floating-point value.
+// (%c) -> Applies CSS style rules to all following text.
+# ---------------------------------------------------------------------------------------------
+? data types in javaScript:
+! we can use the TypeOf operator to know the type of any data e.g. (typeOf 500).
+// primitive data types:
+~ 1) string -> "Ahmad"
+~ 2) number -> 2000 or 2000.998
+~ 3) boolean -> true or false
+~ 4) bigint -> 
+~ 5) symbol -> 
+~ 6) null -> its an object
+~ 7) undefined
+// non-primitive data types:
+~ 1) object -> it could be array or an regular object.
+~ 2) function -> function FunName(){}.
+*/
